cc6b5b13ed02d48641552298844fe25d333193ee
Md Kaif <mdkaif0153@gmail.com>
2025-09-29 19:58:05 +0530

    Update error handling in controllers

--- src/controllers/places.controller.js
+++ src/controllers/places.controller.js
@@ -1,38 +1,82 @@
 const placesModel = require("../models/places.model");
 
 async function showPlaces(req, res) {
-  const allPlaces = await placesModel.find({});
-  res.render("places/index", { allPlaces });
+  try {
+    const allPlaces = await placesModel.find({});
+    if (!allPlaces) {
+      throw new Error("No Places found!");
+    }
+    res.render("places/index", { allPlaces });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function singlePlace(req, res) {
-  const place = await placesModel.findById(req.params.id);
-  res.render("places/show", { place });
+  try {
+    const place = await placesModel.findById(req.params.id);
+    if (!place) {
+      throw new Error("Couldn't find the place!");
+    }
+    res.render("places/show", { place });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 function newPlaceForm(req, res) {
   res.render("places/add");
 }
 
-async function addNewPlace(req,res){
+async function addNewPlace(req, res) {
+  try {
     const place = await placesModel.create(req.body.place);
+    if (!place) {
+      throw new Error("Something went wrong!");
+    }
     res.redirect(`places/${place._id}`);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function showEditPlace(req,res){
+async function showEditPlace(req, res) {
+  try {
     const place = await placesModel.findById(req.params.id);
-    res.render('places/edit', {place})
+    if (!place) {
+      throw new Error("Something went wrong!");
+    }
+    res.render("places/edit", { place });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function editPlace(req,res){
-    const updatedPlace = await placesModel.findByIdAndUpdate(req.params.id,{...req.body.place});
+async function editPlace(req, res) {
+  try {
+    const updatedPlace = await placesModel.findByIdAndUpdate(req.params.id, {
+      ...req.body.place,
+    });
+    if (!updatedPlace) {
+        throw new Error("Couldn't update place!");
+    }
     res.redirect(`/places/${updatedPlace._id}`);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function deletePlace(req,res){
-    await placesModel.findByIdAndDelete(req.params.id);
-    res.redirect('/places')
+async function deletePlace(req, res) {
+  await placesModel.findByIdAndDelete(req.params.id);
+  res.redirect("/places");
 }
 
-
-module.exports = { showPlaces, singlePlace, newPlaceForm, addNewPlace,showEditPlace, editPlace, deletePlace };
+module.exports = {
+  showPlaces,
+  singlePlace,
+  newPlaceForm,
+  addNewPlace,
+  showEditPlace,
+  editPlace,
+  deletePlace,
+};
